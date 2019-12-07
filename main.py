bb2b31e2b4c00fff99f7b89c78ee1c742dd4f14d
Camille Brett <camille.bretteville@gmail.com>
2019-12-07 23:56:43 +0100

    Format main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Simulation du canal
+# Simulation du canal
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,62 +9,63 @@ from src.sequence import generate_random_sequence
 from src.canal import gaussian_noise, awgn
 from src.viterbi import viterbi
 
-#Frequences en Hz (modifiable)
+# Frequences en Hz (modifiable)
 f_porteuse = 100.
 f_symboles = 40.
 f_ech = 8*f_porteuse
 
-#SNR en dB (modifiable)
+# SNR en dB (modifiable)
 gammadB = 15
 
-#Nombre de bits dans le message transmis (modifiable)
+# Nombre de bits dans le message transmis (modifiable)
 nb_symboles = 200
 
-#Nombre de points d'echantillonnage
+# Nombre de points d'echantillonnage
 nb_points = int(nb_symboles*f_ech/f_symboles)
 
 nb_ech_per_symbole = int(f_ech/f_symboles)
 
-#temps
+# temps
 time_fech = np.arange(0, nb_symboles/f_symboles, 1/f_ech)
 
-#Generation du message a envoyer
+# Generation du message a envoyer
 message = generate_random_sequence(nb_symboles)
 
-#Codage du message pour le rendre resistant au canal
+# Codage du message pour le rendre resistant au canal
 encoded_message = crc_encoding(message)
 
-#Modulation du message: on passe sur un signal physique
+# Modulation du message: on passe sur un signal physique
 porteuse, modulant, signal_module = ask_modulate(
     encoded_message, nb_symboles, f_porteuse, f_symboles, f_ech)
 
 
-#Calcul de l'energie par bits du signal
+# Calcul de l'energie par bits du signal
 Eb = sum([signal_module[i]**2 for i in range(len(signal_module))])/nb_symboles
 
-#Calcul de la puissance du bruit
+# Calcul de la puissance du bruit
 N0 = Eb/(10**(gammadB/10))
 
-#Calcul de l'ecart type du bruit
+# Calcul de l'ecart type du bruit
 sigma = np.sqrt(N0/2)
 
-#Le signal passe dans le canal
+# Le signal passe dans le canal
 noise, noisy_signal = awgn(signal_module, sigma)
 
-#On demodule le signal
+# On demodule le signal
 received_message, received_amplitude = ask_demodulate(
     noisy_signal, nb_symboles, nb_ech_per_symbole)
 
-#On decode le message
+# On decode le message
 decoded_message = viterbi(received_message)
 
-#On calcule le nombre d'erreurs et le TEB et le TES
+# On calcule le nombre d'erreurs et le TEB et le TES
 nb_errors = sum([message[i] != decoded_message[i] for i in range(nb_symboles)])
-nb_errors_symb = sum([encoded_message[i] != received_message[i] for i in range(nb_symboles)])
+nb_errors_symb = sum([encoded_message[i] != received_message[i]
+                      for i in range(nb_symboles)])
 TEB = nb_errors/nb_symboles
 TES = nb_errors_symb/nb_symboles
 
-#On affiche des informations dans la console
+# On affiche des informations dans la console
 print("Taille du message envoyé : ", nb_symboles, "bits")
 print("Fréquence porteuse : ", f_porteuse, "Hz; ", "Fréquence d'échantillonnage : ",
       f_ech, "Hz; ", "Fréquence d'émission symbole : ", f_symboles, "Hz; ")
@@ -77,44 +78,44 @@ decoded_message_plot = []
 
 for i in range(nb_symboles):
     message_plot[i*nb_ech_per_symbole:(i+1)*nb_ech_per_symbole] = [message[i]
-                                                                    for j in range(nb_ech_per_symbole)]
+                                                                   for j in range(nb_ech_per_symbole)]
     decoded_message_plot[i*nb_ech_per_symbole:(i+1)*nb_ech_per_symbole] = [
         decoded_message[i] for j in range(nb_ech_per_symbole)]
 
-#Figures : message initial, message code, signal module, signal bruite, message code recu (signal demodule), message decode
+# Figures : message initial, message code, signal module, signal bruite, message code recu (signal demodule), message decode
 
 plt.figure("SNR = " + str(gammadB) + "dB; " + "TEB = " + str(TEB))
-plt.subplot(3,2,1)
+plt.subplot(3, 2, 1)
 plt.plot(time_fech, message_plot)
 plt.xlabel("Temps (s)")
 plt.ylabel("Etat")
 plt.title("Information transmise")
 
-plt.subplot(3,2,2)
+plt.subplot(3, 2, 2)
 plt.plot(time_fech, modulant)
 plt.xlabel("Temps (s)")
 plt.ylabel("Etat")
 plt.title("Information codée")
 
-plt.subplot(3,2,3)
+plt.subplot(3, 2, 3)
 plt.plot(time_fech, signal_module, lw=0.5)
 plt.xlabel("Temps (s)")
 plt.ylabel("Amplitude (V)")
 plt.title("Signal modulé")
 
-plt.subplot(3,2,4)
+plt.subplot(3, 2, 4)
 plt.plot(time_fech, noisy_signal, lw=0.5)
 plt.xlabel("Temps (s)")
 plt.ylabel("Amplitude (V)")
 plt.title("Signal reçu après canal")
 
-plt.subplot(3,2,5)
+plt.subplot(3, 2, 5)
 plt.plot(time_fech, received_amplitude)
 plt.xlabel("Temps (s)")
 plt.ylabel("Etat")
 plt.title("Message codé reçu")
 
-plt.subplot(3,2,6)
+plt.subplot(3, 2, 6)
 plt.plot(time_fech, decoded_message_plot)
 plt.xlabel("Temps (s)")
 plt.ylabel("Etat")
@@ -122,4 +123,3 @@ plt.title("Information reçue (décodée)")
 
 plt.tight_layout()
 plt.show()
-
