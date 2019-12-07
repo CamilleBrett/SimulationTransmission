918e6f3d4ebe9a57fb89536e8c70ab44b5960c29
Camille Brett <camille.bretteville@gmail.com>
2019-12-07 23:27:20 +0100

    Add doc to sequence.py

--- src/sequence.py
+++ src/sequence.py
@@ -1,6 +1,8 @@
-import numpy as np 
+# Genere un message aleatoire
+
+import numpy as np
 
 
 def generate_random_sequence(n_bits):
+    """Genere un message aleatoire de n_bits bits"""
     return list(np.random.randint(0, 2, n_bits))
-
